b5f617fe87005ab7cfcf43154fff3df546317a77
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-10-20 19:41:49 -0400

    implement data edit and upload functions
    implement data edit: load current data from api with useQuery
    data upload: useMutation to handle upload function, UI update and fallback

--- urrqs24viteapp/src/components/Events/EditEvent.tsx
+++ urrqs24viteapp/src/components/Events/EditEvent.tsx
@@ -1,23 +1,87 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import Modal from "../UI/Modal.js";
 import EventForm from "./EventForm.jsx";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import {
+  getEvent,
+  uploadEditedEvent,
+  queryClient,
+  ErrorT,
+} from "../../helper/httpRequest.js";
+import ErrorBlock from "../UI/ErrorBlock.js";
+import LoadingIndicator from "../UI/LoadingIndicator.js";
 
 export default function EditEvent() {
   const navigate = useNavigate();
 
-  const handleSubmit = (formData: any) => {
-    console.log(formData);
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => getEvent({ signal, id: params.id }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: uploadEditedEvent,
+    onMutate: async (data) => {
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const lastVerEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueryData(["events", params.id], data.event);
+
+      return { lastVerEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context?.lastVerEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["events", params.id] });
+    },
+  });
+
+  const editedEventSubmitHandler = (formData: any) => {
+    mutate({ id: params.id!, event: formData });
+    navigate("../");
   };
 
   const handleClose = () => {
     navigate("../");
   };
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={undefined} onSubmit={handleSubmit}>
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            (error as ErrorT).info?.message ||
+            "Failed to load event. Please check your inputs and try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={editedEventSubmitHandler}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -25,6 +89,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
+    );
+  }
+
+  return (
+    <div id="modal">
+      <Modal onClose={handleClose}>{content}</Modal>
+    </div>
   );
 }
--- urrqs24viteapp/src/helper/httpRequest.tsx
+++ urrqs24viteapp/src/helper/httpRequest.tsx
@@ -118,3 +118,25 @@ export const deleteEvent = async (props: { id: string }) => {
 
   return response.json();
 };
+
+export const uploadEditedEvent = async (props: { id: string; event: any }) => {
+  const { id, event } = props;
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ event }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error: ErrorT = new Error(
+      "An error occurred while updating the event"
+    );
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+};
