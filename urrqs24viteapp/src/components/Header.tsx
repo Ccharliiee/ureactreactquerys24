169064115b1baa4aced96b33b0d00d4591a98fe6
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-10-22 21:13:48 -0400

    Update Header.tsx

--- urrqs24viteapp/src/components/Header.tsx
+++ urrqs24viteapp/src/components/Header.tsx
@@ -5,6 +5,8 @@ import {
   ReactPortal,
 } from "react";
 
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header(props: {
   children:
     | string
@@ -16,9 +18,12 @@ export default function Header(props: {
     | null
     | undefined;
 }) {
+  const fetchingInProgress = useIsFetching();
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">
+        {fetchingInProgress > 0 && <progress />}
+      </div>
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
